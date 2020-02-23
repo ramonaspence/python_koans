795324ad27206f91aac693ada151288a3da6ca58
RamonaEJSpence <ramonaejspence@gmail.com>
2020-02-22 19:25:52 -0500

    went back and did about triangle project correctly. And now I'm working on triangle project 2

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -17,8 +17,19 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    if a == b and b == c:
+        return 'equilateral'
+    elif (a == b):
+        return 'isosceles'
+    elif (b == c):
+        return 'isosceles'
+    elif (a == c):
+        return 'isosceles'
+    elif:
+        return 'scalene'
+
+
+
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
